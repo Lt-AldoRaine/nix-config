2bd9f026f96013c9d3609fd2ca80d94ee9403781
Connor Pennington <harambefallon@gmail.com>
2025-02-13 09:00:54 +0000

    Add ui stuff and harpoon to nvim

--- home/programs/nvim/plugins/utils.nix
+++ home/programs/nvim/plugins/utils.nix
@@ -1,13 +1,23 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   programs.nixvim = {
+	home.packages = with pkgs; [ ctags ];
+
     highlightOverride = {
       FloatBorder.fg = "#${config.lib.stylix.colors.base0D}";
     };
-    nixpkgs.config = { allowUnfree = true; };
     plugins = {
       tmux-navigator.enable = true;
       comment.enable = true;
       nvim-autopairs.enable = true;
     };
+
+    web-devicons.enable = true;
+    noice.enable = true;
+    gitsigns = {
+      enable = true;
+      settings.current_line_blame = false;
+    };
+    trouble.enable = true;
+    bufferline.enable = true;
   };
 }
