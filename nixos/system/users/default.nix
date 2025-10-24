d93cf60c0304cf7d3e3ca93b5f5d444ce4779074
Connor Pennington <harambefallon@gmail.com>
2025-10-24 03:08:44 -0500

    .

--- nixos/system/users/default.nix
+++ nixos/system/users/default.nix
@@ -11,6 +11,8 @@ in {
       extraGroups = [ "wheel" "networkmanager" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKPhGbF2NAnxBf12gGtNryqnWUv5KVywc3JroO+f40J5 connor@aldoraine"
+
+				"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAUbHbQvRblFbKll9PxVzwiwW3PZsPYULJdiIsqHItgU connor@homelab"
       ];
 
     };
