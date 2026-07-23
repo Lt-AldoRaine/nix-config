bbc8778c3f00b90169430b7e7e90b3e5bc62618e
Connor Pennington <harambefallon@gmail.com>
2026-07-22 21:55:11 -0500

    homelab: remove jellyfin-web skip-intro overlay

--- configuration/hosts/homelab/configuration.nix
+++ configuration/hosts/homelab/configuration.nix
@@ -32,19 +32,6 @@
     ./hardware-configuration.nix
     ./variables.nix
   ];
-  nixpkgs.overlays = [
-    (final: prev: {
-      jellyfin-web = prev.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
-        installPhase = ''
-          runHook preInstall
-          sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
-          mkdir -p $out/share
-          cp -a dist $out/share/jellyfin-web
-          runHook postInstall
-        '';
-      });
-    })
-  ];
 	 
   services.my-caddy.enable = true;
   services.authelia.instances.main.enable = true;
