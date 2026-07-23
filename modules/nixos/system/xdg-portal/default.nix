2527154c8a57de9cdae911a8616a9bcef973af5f
Connor Pennington <harambefallon@gmail.com>
2026-07-22 21:55:12 -0500

    xdg-portal: remove stale OpenURI comment

--- modules/nixos/system/xdg-portal/default.nix
+++ modules/nixos/system/xdg-portal/default.nix
@@ -3,10 +3,6 @@
     enable = true;
     config.common.default = "*";
     wlr.enable = true;
-    # No installed portal backend (hyprland, gtk) implements OpenURI - only
-    # xdg-desktop-portal-gnome does, which is too heavy for this setup. Forcing
-    # xdg-open through the portal here just breaks it with "No such interface
-    # org.freedesktop.portal.OpenURI". Let xdg-open resolve mimeapps.list directly.
     xdgOpenUsePortal = false;
     extraPortals = with pkgs; 
 			[ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
