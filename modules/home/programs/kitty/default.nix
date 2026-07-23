f9c0f489c849d898b25c64c05f9818dc75b73136
Connor Pennington <harambefallon@gmail.com>
2026-07-22 21:55:12 -0500

    kitty: include colors.conf and add gtk3/gtk4 theme css

--- modules/home/programs/kitty/default.nix
+++ modules/home/programs/kitty/default.nix
@@ -1,21 +1,32 @@
-{ lib, ... }: {
+{ lib, config, ... }: {
   programs.kitty = lib.mkMerge [
     { enable = lib.mkDefault true; }
-{
-    settings = {
-      scrollback_lines = 10000;
-      initial_window_width = 1200;
-      initial_window_height = 600;
-      update_check_interval = 0;
-      enable_audio_bell = false;
-      confirm_os_window_close = "0";
-      remember_window_size = "no";
-      disable_ligatures = "never";
-      url_style = "curly";
-      cursor_shape = "Underline";
-      cursor_underline_thickness = 3;
-      window_padding_width = 10;
-    };
+    {
+      extraConfig = "include colors.conf";
+      settings = {
+        scrollback_lines = 10000;
+        initial_window_width = 1200;
+        initial_window_height = 600;
+        update_check_interval = 0;
+        enable_audio_bell = false;
+        confirm_os_window_close = "0";
+        remember_window_size = "no";
+        disable_ligatures = "never";
+        url_style = "curly";
+        cursor_shape = "Underline";
+        cursor_underline_thickness = 3;
+        window_padding_width = 10;
+      };
     }
   ];
+
+  gtk = {
+    enable = lib.mkDefault true;
+    gtk3.extraCss = ''
+      @import url("file://${config.home.homeDirectory}/.config/gtk-3.0/colors.css");
+    '';
+    gtk4.extraCss = ''
+      @import url("file://${config.home.homeDirectory}/.config/gtk-4.0/colors.css");
+    '';
+  };
 }
